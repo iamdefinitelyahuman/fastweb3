17475eff2d2882796f5b5e2afd5fbea88058e1bf
iamdefinitelyahuman <ben@hauser.id>
2026-02-19 21:25:11 +0000

    feat: expose Web3 in top-level init

--- src/fastweb3/__init__.py
+++ src/fastweb3/__init__.py
@@ -1,3 +1,12 @@
 """fastweb3 - High-performance Web3 client."""
 
-__all__ = []
+from .errors import FastWeb3Error, RPCError, TransportError
+from .web3 import Web3, Web3Config
+
+__all__ = [
+    "Web3",
+    "Web3Config",
+    "FastWeb3Error",
+    "RPCError",
+    "TransportError",
+]
